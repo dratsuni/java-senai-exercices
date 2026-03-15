9f889b3c9d815996fc65353bb72ad49cdd5da856
dratsuni <241861453+dratsuni@users.noreply.github.com>
2026-03-14 21:52:27 -0300

    pequenas correções.

--- src/exerciciocontabancaria/Program.java
+++ src/exerciciocontabancaria/Program.java
@@ -8,7 +8,7 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Conta conta = ContaView.criarConta(sc);
-		
-		while(Menu.menu(ContaView.menuConta(sc, conta), sc, conta));	
+		while(Menu.menu(ContaView.menuConta(sc, conta), sc, conta));
+		sc.close();
 	}
 }
